7ad27447f0491587f332505e8a00c54a8025a44f
Sebastian Głowacki <glowackiseb@interia.pl>
2023-01-17 15:38:47 +0100

    Initial spring and angular

---
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/WebStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.porfolio.webstorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreSpringApplication.class, args);
+    }
+
+}
