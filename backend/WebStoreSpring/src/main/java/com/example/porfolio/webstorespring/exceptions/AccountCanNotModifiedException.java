0d5d00dcacd3747402b4ca5ef612d8381a798d9d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-03-09 17:58:09 +0100

    test: AccountDetailsServiceTest; change: AccountCanNotModifiedException message

--- backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/exceptions/AccountCanNotModifiedException.java
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/exceptions/AccountCanNotModifiedException.java
@@ -3,6 +3,6 @@ package com.example.porfolio.webstorespring.exceptions;
 public class AccountCanNotModifiedException extends RuntimeException {
 
     public AccountCanNotModifiedException() {
-        super(String.format("You can only modified your own account!"));
+        super(String.format("You can only used or modified your own account!"));
     }
 }
