cdeb8c4f4f8e3dc56ce739b636cb23bbe7078796
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-04-06 19:55:43 +0200

    feature: logout

---
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/services/accounts/LogoutService.java
@@ -0,0 +1,36 @@
+package com.example.porfolio.webstorespring.services.accounts;
+
+import com.example.porfolio.webstorespring.model.entity.accounts.AuthToken;
+import com.example.porfolio.webstorespring.repositories.accounts.AuthTokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    private final AuthTokenRepository authTokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        AuthToken storedToken = authTokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            authTokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
+    }
+}
