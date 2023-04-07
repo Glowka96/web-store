99b34453a1dbae7795619e7c3703f9914334dbad
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-04-08 00:55:53 +0200

    test: LogoutController method

--- backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/accounts/AuthenticationResponse.java
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/accounts/AuthenticationResponse.java
@@ -1,14 +1,10 @@
 package com.example.porfolio.webstorespring.model.dto.accounts;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class AuthenticationResponse {
 
     private String token;
