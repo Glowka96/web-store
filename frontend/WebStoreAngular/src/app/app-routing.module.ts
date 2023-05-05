cd470d8aa81c096bfe95dbdb503f5721774b1fe6
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-05-05 16:29:49 +0200

    fix: quantity of products, feature: product description on hover img

--- frontend/WebStoreAngular/src/app/app-routing.module.ts
+++ frontend/WebStoreAngular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BoardAdminComponent } from './components/board-admin/board-admin.component';
 import { ProductsComponent } from './components/products/products.component';
+import { AccountComponent } from './components/account/account.component';
 
 const routes: Routes = [
   { path: ':subcategoryName/:id/products', component: ProductsComponent },
   { path: 'search', component: ProductsComponent },
   { path: 'admin-board', component: BoardAdminComponent },
+  { path: 'accounts', component: AccountComponent },
 ];
 
 @NgModule({
