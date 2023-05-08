3ce11eda682241493a74ba2f812bb17957416ddb
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-05-08 18:01:42 +0200

    feature: components: account, footer, order and basket style; fix: background on the web; change: tag html, renamed shipment's field

--- frontend/WebStoreAngular/src/app/services/shop.service.ts
+++ frontend/WebStoreAngular/src/app/services/shop.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { Product } from '../models/product';
 import { Shipment } from '../models/shipment';
 
 @Injectable({
@@ -10,23 +9,24 @@ import { Shipment } from '../models/shipment';
 })
 export class ShopService {
   private apiServerUrl = environment.apiBaseUrl;
-  public basket: BehaviorSubject<Shipment[]> = new BehaviorSubject(
+  private basket: BehaviorSubject<Shipment[]> = new BehaviorSubject(
     [] as Shipment[]
   );
   constructor(private http: HttpClient) {}
 
   addToBasket(shipment: Shipment) {
-    console.log('start methods');
     let cart = this.basket.value;
-    console.log(cart);
     let findShipment = cart.find((s) => s.product.id == shipment.product.id);
     if (findShipment) {
       findShipment.price = findShipment.price + shipment.price;
-      findShipment.quality += shipment.quality;
+      findShipment.quantity += shipment.quantity;
     } else {
       cart.push(shipment);
     }
     this.basket.next(cart);
-    console.log(cart);
+  }
+
+  public get basket$() {
+    return this.basket;
   }
 }
