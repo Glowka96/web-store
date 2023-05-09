3ffbb47b3f6e0693b289ca4585354ae8dcd3de58
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-05-09 16:03:15 +0200

    feature: modification basket

--- frontend/WebStoreAngular/src/app/components/basket/basket.component.ts
+++ frontend/WebStoreAngular/src/app/components/basket/basket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Shipment } from 'src/app/models/shipment';
 import { ShopService } from 'src/app/services/shop.service';
 
@@ -8,7 +9,15 @@ import { ShopService } from 'src/app/services/shop.service';
   styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent implements OnInit {
-  private basket?: Shipment[];
+  private basket: Shipment[] = [];
+  private update: boolean = false;
+
+  public changeForm = new FormGroup({
+    quantity: new FormControl('', {
+      validators: [Validators.min(1), Validators.max(100)],
+      updateOn: 'change',
+    }),
+  });
 
   constructor(private shopService: ShopService) {
     this.shopService.basket$.subscribe((shipments) => {
@@ -21,5 +30,40 @@ export class BasketComponent implements OnInit {
 
   public get shipments() {
     return this.basket;
+  }
+
+  public get isBasketEmpty() {
+    return this.basket?.length > 0;
+  }
+
+  public deleteProductFromBasket(productId: string) {
+    let index = this.getIndexBasket(productId);
+    this.basket.splice(index, 1);
+  }
+
+  public isUpdate() {
+    return this.update;
+  }
+
+  public change() {
+    this.update = !this.update;
+  }
+
+  public onSumbitChange(productId: string) {
+    if (this.changeForm.valid) {
+      let index = this.getIndexBasket(productId);
+      let quantity = this.changeForm.controls['quantity']?.value;
+
+      this.basket[index].quantity = Number(quantity);
+      this.basket[index].price =
+        Number(quantity) * Number(this.basket[index].product.price);
+      this.change();
+    }
+  }
+
+  private getIndexBasket(productId: string) {
+    return this.basket.findIndex((shipment) => {
+      return shipment.product.id === productId;
+    });
   }
 }
