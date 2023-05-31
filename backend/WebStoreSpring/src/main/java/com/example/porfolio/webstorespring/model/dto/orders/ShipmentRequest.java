f3e808b1d483040260bf8154a217c79b0c071c02
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-05-31 18:34:09 +0200

    feature: home page, dto fields annotations

--- backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/orders/ShipmentRequest.java
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/orders/ShipmentRequest.java
@@ -2,6 +2,9 @@ package com.example.porfolio.webstorespring.model.dto.orders;
 
 import com.example.porfolio.webstorespring.model.dto.products.ProductRequest;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -12,7 +15,11 @@ public class ShipmentRequest {
     @JsonProperty("product")
     private ProductRequest productRequest;
 
+    @NotNull(message = "The quantity can't be null")
+    @Min(value = 1, message = "The quantity must be greater than or equal to one")
+    @Max(value = 100, message = "The quantity must be less than or equal to hundred")
     private Integer quantity;
 
+    @Digits(integer = 8, fraction = 2, message = "The price is invalid")
     private Double price;
 }
