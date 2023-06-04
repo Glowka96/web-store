72befee8562d9dad9e820d2db35b61da138b55bd
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-06-04 12:20:00 +0200

    change: json fields name, img loading and size, input label, form input background color, corrected class name, test: add new object and corrected fields

--- backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/orders/ShipmentResponse.java
+++ backend/WebStoreSpring/src/main/java/com/example/porfolio/webstorespring/model/dto/orders/ShipmentResponse.java
@@ -1,6 +1,7 @@
 package com.example.porfolio.webstorespring.model.dto.orders;
 
 import com.example.porfolio.webstorespring.model.dto.products.ProductResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -8,6 +9,7 @@ public class ShipmentResponse {
 
     private Long id;
 
+    @JsonProperty("product")
     private ProductResponse productResponse;
 
     private Integer quantity;
