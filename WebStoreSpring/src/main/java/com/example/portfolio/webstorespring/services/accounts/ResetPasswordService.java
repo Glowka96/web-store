eabcee0ef19a94f0d41a0906cc5c9c0ae6bf6ba5
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-09-11 17:14:30 +0200

    fix: instantiating multiple means of the same class - removed @Lombok dependency injection

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/accounts/ResetPasswordService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/accounts/ResetPasswordService.java
@@ -7,7 +7,7 @@ import com.example.portfolio.webstorespring.model.entity.accounts.Account;
 import com.example.portfolio.webstorespring.model.entity.accounts.ConfirmationToken;
 import com.example.portfolio.webstorespring.repositories.accounts.AccountRepository;
 import com.example.portfolio.webstorespring.services.email.EmailSenderService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,14 +15,23 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor()
 public class ResetPasswordService {
 
     private final BCryptPasswordEncoder encoder;
     private final ConfirmationTokenService confirmationTokenService;
-    @Qualifier("resetPasswordSender")
     private final EmailSenderService emailSenderService;
     private final AccountRepository accountRepository;
+
+    @Autowired
+    public ResetPasswordService(BCryptPasswordEncoder encoder,
+                                ConfirmationTokenService confirmationTokenService,
+                                @Qualifier(value = "resetPasswordSender") EmailSenderService emailSenderService,
+                                AccountRepository accountRepository) {
+        this.encoder = encoder;
+        this.confirmationTokenService = confirmationTokenService;
+        this.emailSenderService = emailSenderService;
+        this.accountRepository = accountRepository;
+    }
 
     public Map<String, Object> resetPasswordByEmail(String email) {
         Account account = accountRepository.findByEmail(email)
