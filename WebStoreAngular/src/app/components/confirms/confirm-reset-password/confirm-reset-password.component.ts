efc620602b3c69b7495fe83cbdf7cdd6aae7193e
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-09-17 15:52:38 +0200

    feature: confirm reset password component, new route link; change: code refactoring (class naming - import style sccs), confirm component naming;

---
+++ WebStoreAngular/src/app/components/confirms/confirm-reset-password/confirm-reset-password.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResetPasswordService } from 'src/app/services/accounts/reset-password.service';
+import { PasswordFormBuilderService } from 'src/app/services/forms/password-form-builder.service';
+
+@Component({
+  selector: 'app-confirm-reset-password',
+  templateUrl: './confirm-reset-password.component.html',
+  styleUrls: ['./confirm-reset-password.component.scss'],
+})
+export class ConfirmResetPasswordComponent implements OnInit {
+  private token!: string;
+  private resposneMessage!: string;
+  public confirmResetPasswordForm!: FormGroup;
+
+  constructor(
+    private resetPasswordService: ResetPasswordService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private passwordFormBuilderService: PasswordFormBuilderService
+  ) {
+    this.confirmResetPasswordForm =
+      passwordFormBuilderService.createPasswordFormGroup();
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.token = params['token'];
+    });
+  }
+
+  onSubmitConfirmResetPassword() {
+    if (this.confirmResetPasswordForm.valid) {
+      const password = this.confirmResetPasswordForm.get('password')?.value;
+      this.resetPasswordService
+        .confirmResetPassword(password, this.token)
+        .subscribe({
+          next: (response) => {
+            this.resposneMessage = response.message;
+            setTimeout(() => {
+              this.router.navigate(['/']);
+            }, 2000);
+          },
+          error: (error) => {
+            const errorMessage = error.error.errors.join('<br>');
+            this.resposneMessage = errorMessage;
+          },
+        });
+    }
+  }
+
+  public get responseMsg() {
+    return this.resposneMessage;
+  }
+}
