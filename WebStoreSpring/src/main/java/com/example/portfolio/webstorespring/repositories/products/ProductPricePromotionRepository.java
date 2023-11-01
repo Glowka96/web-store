829d072dfe7cddcebc3cec8de190aca418953e63
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2023-11-01 19:00:37 +0100

    feature: ProductPricePromotion mapper, repository and service;

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/products/ProductPricePromotionRepository.java
@@ -0,0 +1,7 @@
+package com.example.portfolio.webstorespring.repositories.products;
+
+import com.example.portfolio.webstorespring.model.entity.products.ProductPricePromotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductPricePromotionRepository extends JpaRepository<ProductPricePromotion, Long> {
+}
---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/ProductPricePromotionService.java
@@ -0,0 +1,51 @@
+package com.example.portfolio.webstorespring.services.products;
+
+import com.example.portfolio.webstorespring.exceptions.ResourceNotFoundException;
+import com.example.portfolio.webstorespring.mappers.ProductPricePromotionMapper;
+import com.example.portfolio.webstorespring.model.dto.products.request.ProductPricePromotionRequest;
+import com.example.portfolio.webstorespring.model.dto.products.response.ProductPricePromotionResponse;
+import com.example.portfolio.webstorespring.model.entity.products.Product;
+import com.example.portfolio.webstorespring.model.entity.products.ProductPricePromotion;
+import com.example.portfolio.webstorespring.repositories.products.ProductPricePromotionRepository;
+import com.example.portfolio.webstorespring.repositories.products.ProductRepository;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductPricePromotionService {
+
+    private final ProductPricePromotionRepository promotionRepository;
+    private final ProductRepository productRepository;
+    private final ProductPricePromotionMapper promotionMapper;
+
+    @Transactional
+    public ProductPricePromotionResponse saveProductPricePromotion(@NotNull ProductPricePromotionRequest promotionRequest) {
+        Product product = findProductById(promotionRequest.getProductId());
+
+        ProductPricePromotion promotion = promotionMapper.mapToEntity(promotionRequest);
+        promotion.setProduct(product);
+
+        promotionRepository.save(promotion);
+        return promotionMapper.mapToDto(promotion);
+    }
+
+    @Transactional
+    public void deleteProductPricePromotionById(@NotNull Long id) {
+        ProductPricePromotion promotion = findProductPricePromotionById(id);
+        promotionRepository.delete(promotion);
+    }
+
+    private Product findProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "id", id));
+    }
+
+    private ProductPricePromotion findProductPricePromotionById(Long id) {
+        return promotionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ProductPricePromotion", "id", id));
+    }
+
+}
