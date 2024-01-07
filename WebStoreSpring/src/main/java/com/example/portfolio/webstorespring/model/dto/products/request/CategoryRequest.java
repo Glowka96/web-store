d120ff0e0480333b11aa5649eaa718c99a7a910e
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-01-07 16:46:45 +0100

    fix: HttpMessageNotReadableException -> add @AllArgsConstructor and @NoArgsConstructor annotations to category dtos

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/request/CategoryRequest.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/request/CategoryRequest.java
@@ -2,12 +2,16 @@ package com.example.portfolio.webstorespring.model.dto.products.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequest {
 
     @NotBlank(message = "The category name can't be blank")
--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/response/CategoryResponse.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/response/CategoryResponse.java
@@ -1,13 +1,17 @@
 package com.example.portfolio.webstorespring.model.dto.products.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryResponse {
 
     private Long id;
