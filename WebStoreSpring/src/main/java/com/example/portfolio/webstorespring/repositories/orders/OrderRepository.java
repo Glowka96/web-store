ee8373dd148f752952e2e3bb302db63fbc55bf3d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-01-17 18:01:35 +0100

    feature: findLastFiveAccountOrder in repository

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/orders/OrderRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/orders/OrderRepository.java
@@ -2,12 +2,20 @@ package com.example.portfolio.webstorespring.repositories.orders;
 
 import com.example.portfolio.webstorespring.model.entity.orders.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByAccountId(Long accountId);
 
-    Optional<Order> findOrderByAccountIdAndId(Long accountId, Long orderId);
+    @Query("""
+        SELECT o FROM Order o
+        WHERE o.account.id = :accountId
+        ORDER BY o.dateOfCreation DESC
+        LIMIT 5
+""")
+    List<Order> findLastFiveAccountOrder(@Param(value = "accountId") Long accountId);
+
 }
