2a9b57ff01492ad24d895b046319cb5442d775b9
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-02-21 16:43:00 +0100

    test: method when product has promotion;

--- WebStoreSpring/src/test/java/com/example/portfolio/webstorespring/services/orders/ShipmentServiceTest.java
+++ WebStoreSpring/src/test/java/com/example/portfolio/webstorespring/services/orders/ShipmentServiceTest.java
@@ -18,10 +18,13 @@ import java.util.Optional;
 import static com.example.portfolio.webstorespring.buildhelpers.orders.OrderBuilderHelper.createOrderWithoutShipments;
 import static com.example.portfolio.webstorespring.buildhelpers.orders.ShipmentBuilderHelper.createShipmentRequest;
 import static com.example.portfolio.webstorespring.buildhelpers.products.ProductBuilderHelper.createProduct;
+import static com.example.portfolio.webstorespring.buildhelpers.products.ProductBuilderHelper.createProductWithPromotion;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ShipmentServiceTest {
@@ -51,6 +54,29 @@ class ShipmentServiceTest {
         assertThat(result.get(0).getOrder()).isEqualTo(order);
         assertThat(result.get(0).getProduct().getQuantity())
                 .isNotEqualTo(productQuantity);
+        verify(shipmentRepository, times(1)).saveAll(result);
+    }
+
+    @Test
+    void shouldGetSetupShipmentsWithProductPromotion() {
+        // given
+        Product product = createProductWithPromotion();
+        ShipmentRequest shipmentRequest = createShipmentRequest();
+        Order order = createOrderWithoutShipments();
+        Long productQuantity = product.getQuantity();
+
+        given(productRepository.findProductByIdWithPromotion(anyLong())).willReturn(Optional.of(product));
+
+        // when
+        List<Shipment> result =  underTest.getSetupShipments(order, List.of(shipmentRequest));
+
+        // then
+        assertThat(result).hasSize(1).isNotEmpty();
+        assertThat(result.get(0).getOrder()).isEqualTo(order);
+        assertThat(result.get(0).getProduct().getQuantity())
+                .isNotEqualTo(productQuantity);
+        verify(shipmentRepository, times(1)).saveAll(result);
+
     }
 
     @Test
