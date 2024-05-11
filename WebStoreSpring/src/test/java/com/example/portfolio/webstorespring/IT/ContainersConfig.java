f5823e3b03e17ce00ea0ac9b09db8f1b825277fb
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-05-11 02:29:41 +0200

    feature: spring boot testcontainers dependency and ContainersConfig for tests

---
+++ WebStoreSpring/src/test/java/com/example/portfolio/webstorespring/IT/ContainersConfig.java
@@ -0,0 +1,16 @@
+package com.example.portfolio.webstorespring.IT;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContainersConfig {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mySQLContainer() {
+        return new MySQLContainer<>("mysql:8.0.31");
+    }
+}
