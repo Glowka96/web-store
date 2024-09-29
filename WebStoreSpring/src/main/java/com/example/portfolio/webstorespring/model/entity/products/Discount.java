8747ddc7249a89bbe9155250d80eb8c7de8352df
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-09-29 16:44:11 +0200

    feature: Discount; change: add equals&hasCode in Subcategory

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/entity/products/Discount.java
@@ -0,0 +1,36 @@
+package com.example.portfolio.webstorespring.model.entity.products;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Discount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String code;
+
+    @Column(nullable = false, updatable = false)
+    private BigDecimal discountRate;
+
+    @Column(nullable = false, updatable = false)
+    private Long quantity;
+
+    @ManyToMany
+    @JoinTable(name = "discount_subcategory",
+            joinColumns = @JoinColumn(name = "discount_id"),
+            inverseJoinColumns = @JoinColumn(name = "subcategory_id"))
+    private Set<Subcategory> subcategories;
+}
