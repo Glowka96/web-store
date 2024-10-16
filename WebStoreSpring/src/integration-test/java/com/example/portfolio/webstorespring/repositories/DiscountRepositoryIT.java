554375a8453fdfa0c5ac57995271ce43d98194ed
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-10-16 19:43:48 +0200

    feature: find discount by code method in repository; test: integration test of this repository method

---
+++ WebStoreSpring/src/integration-test/java/com/example/portfolio/webstorespring/repositories/DiscountRepositoryIT.java
@@ -0,0 +1,55 @@
+package com.example.portfolio.webstorespring.repositories;
+
+import com.example.portfolio.webstorespring.ContainersConfig;
+import com.example.portfolio.webstorespring.buildhelpers.products.SubcategoryBuilderHelper;
+import com.example.portfolio.webstorespring.model.entity.products.Discount;
+import com.example.portfolio.webstorespring.model.entity.products.Subcategory;
+import com.example.portfolio.webstorespring.repositories.products.DiscountRepository;
+import com.example.portfolio.webstorespring.repositories.products.SubcategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Import({ContainersConfig.class})
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DiscountRepositoryIT {
+
+    @Autowired
+    private DiscountRepository discountRepository;
+    @Autowired
+    private SubcategoryRepository subcategoryRepository;
+
+    @BeforeEach
+    void initTestData() {
+        Subcategory subcategory = subcategoryRepository.save(SubcategoryBuilderHelper.createSubcategory());
+        subcategoryRepository.save(Subcategory.builder().name("other").build());
+        discountRepository.save(Discount.builder()
+                        .code("test")
+                        .discountRate(BigDecimal.valueOf(0.1))
+                        .quantity(10L)
+                        .subcategories(Set.of(subcategory))
+                .build());
+    }
+
+    @Test
+    void shouldGetDiscountCodeByCode() {
+        Optional<Discount> optionalDiscount = discountRepository.findByCode("test");
+
+        assertTrue(optionalDiscount.isPresent());
+        assertEquals("test", optionalDiscount.get().getCode());
+        assertEquals(1, optionalDiscount.get().getSubcategories().size());
+    }
+}
---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/products/DiscountRepository.java
@@ -0,0 +1,18 @@
+package com.example.portfolio.webstorespring.repositories.products;
+
+import com.example.portfolio.webstorespring.model.entity.products.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
+
+    @Query("""
+        SELECT d FROM Discount d
+        LEFT JOIN FETCH d.subcategories
+        WHERE d.code = :code
+""")
+    Optional<Discount> findByCode(@Param("code") String code);
+}
