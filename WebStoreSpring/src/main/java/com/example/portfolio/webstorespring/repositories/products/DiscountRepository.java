2dac4922f7c9ca5a1522c96279750a85a67e1d70
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-10-31 18:56:27 +0100

    change: and more conditions in query SQL for findByCode;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/products/DiscountRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/products/DiscountRepository.java
@@ -2,6 +2,7 @@ package com.example.portfolio.webstorespring.repositories.products;
 
 import com.example.portfolio.webstorespring.model.entity.products.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,10 +11,11 @@ import java.util.Optional;
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
     @Query("""
-        SELECT d FROM Discount d
-        LEFT JOIN FETCH d.subcategories
-        WHERE d.code = :code
-""")
+            SELECT d FROM Discount d
+            LEFT JOIN FETCH d.subcategories
+            WHERE d.code = :code
+            AND (d.quantity > 0 AND (d.endDate IS NULL OR d.endDate >= CURRENT_DATE))
+            """)
     Optional<Discount> findByCode(@Param("code") String code);
 
     boolean existsByCode(String code);
