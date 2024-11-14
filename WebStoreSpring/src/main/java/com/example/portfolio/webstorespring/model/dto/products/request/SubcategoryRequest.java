bb1f5add15ffc87c9ef5ac47bf82d80d700fe22d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-11-14 15:31:30 +0100

    change: corrected validation annotation

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/request/SubcategoryRequest.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/model/dto/products/request/SubcategoryRequest.java
@@ -1,7 +1,6 @@
 package com.example.portfolio.webstorespring.model.dto.products.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,8 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SubcategoryRequest {
 
-    @NotNull(message = "The subcategory name can't be null")
-    @NotBlank(message = "The subcategory name can't be blank")
+    @NotBlank(message = "The subcategory name can't be null")
     @Size(min = 3, max = 20, message = "The subcategory name must between min 3 and max 20 letters")
     private String name;
 }
