ba5de76de99b4d089308ccce8cb959edec365515
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-01 14:35:11 +0100

    feature: logs for Role service and initializer;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/accounts/RoleService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/accounts/RoleService.java
@@ -3,12 +3,14 @@ package com.example.portfolio.webstorespring.services.accounts;
 import com.example.portfolio.webstorespring.model.entity.accounts.Role;
 import com.example.portfolio.webstorespring.repositories.accounts.RoleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 class RoleService {
 
     private final RoleRepository roleRepository;
@@ -18,11 +20,14 @@ class RoleService {
     }
 
     void initializeRole(String roleName) {
+        log.debug("Checkin if role exist by name: {}",  roleName);
         if (!roleRepository.existsByName(roleName)) {
+            log.debug("Role doesn't exist.");
             Role role = Role.builder()
                     .name(roleName)
                     .build();
             roleRepository.save(role);
+            log.debug("Saved role.");
         }
     }
 }
