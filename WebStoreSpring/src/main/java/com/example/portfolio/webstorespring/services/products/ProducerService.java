56719273465425ad891cb7d924266a79da6c1352
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-02 18:48:47 +0100

    feature: logs for Producer service;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/ProducerService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/ProducerService.java
@@ -7,6 +7,7 @@ import com.example.portfolio.webstorespring.model.dto.products.response.Producer
 import com.example.portfolio.webstorespring.model.entity.products.Producer;
 import com.example.portfolio.webstorespring.repositories.products.ProducerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,33 +15,41 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProducerService {
 
     private final ProducerRepository producerRepository;
 
     public ProducerResponse getById(Long id) {
+        log.info("Fetching producer for ID: {}", id);
         return ProducerMapper.mapToDto(findById(id));
     }
 
     public List<ProducerResponse> getAll() {
+        log.info("Fetching all producer.");
         return ProducerMapper.mapToDto(producerRepository.findAll());
     }
 
     public ProducerResponse save(ProducerRequest producerRequest) {
+        log.info("Saving producer from request: {}", producerRequest);
         Producer producer = ProducerMapper.mapToEntity(producerRequest);
         producerRepository.save(producer);
+        log.info("Saved producer.");
         return ProducerMapper.mapToDto(producer);
     }
 
     @Transactional
     public ProducerResponse update(Long id, ProducerRequest producerRequest) {
+        log.info("Updating producer. Finding producer for ID: {}", id);
         Producer foundProducer = findById(id);
         foundProducer.setName(producerRequest.name());
         producerRepository.save(foundProducer);
+        log.info("Updated producer.");
         return ProducerMapper.mapToDto(foundProducer);
     }
 
     public void deleteById(Long id) {
+        log.info("Deleting producer for ID: {}", id);
         producerRepository.deleteById(id);
     }
 
