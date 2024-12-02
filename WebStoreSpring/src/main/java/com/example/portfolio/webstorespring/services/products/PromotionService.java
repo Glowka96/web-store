b60cbd5a656a54dff8903bbccd4e4f7680238722
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-02 19:27:27 +0100

    feature: logs for Promotion service;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/PromotionService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/PromotionService.java
@@ -10,11 +10,13 @@ import com.example.portfolio.webstorespring.model.entity.products.Promotion;
 import com.example.portfolio.webstorespring.repositories.products.PromotionRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PromotionService {
 
     private final PromotionRepository promotionRepository;
@@ -22,6 +24,8 @@ public class PromotionService {
 
     @Transactional
     public PromotionResponse save(@NotNull PromotionRequest promotionRequest) {
+        log.info("Saving promotion from request: {}", promotionRequest);
+        log.debug("Fetching product for ID: {}", promotionRequest.productId());
         Product product = productService.findWithPromotionById(promotionRequest.productId());
 
         validateProduct(promotionRequest, product);
@@ -30,14 +34,17 @@ public class PromotionService {
         promotion.setProduct(product);
 
         promotionRepository.save(promotion);
+        log.info("Saved promotion.");
         return PromotionMapper.mapToDto(promotion);
     }
 
     private void validateProduct(PromotionRequest promotionRequest, Product product) {
+        log.debug("Validating if product has promotion.");
         if (!product.getPromotions().isEmpty()) {
             throw new ProductHasAlreadyPromotionException();
         }
 
+        log.debug("Validating if product price is greater promotion price.");
         if(product.getPrice().compareTo(promotionRequest.promotionPrice()) < 0) {
             throw new PromotionPriceGreaterThanBasePriceException();
         }
