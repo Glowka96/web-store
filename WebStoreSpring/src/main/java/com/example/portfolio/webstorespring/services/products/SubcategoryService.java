0a12aea2cc3c4041c009b706b7f38f1911a3292f
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-02 19:31:36 +0100

    feature: logs for Subcategory service;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/SubcategoryService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/products/SubcategoryService.java
@@ -8,6 +8,7 @@ import com.example.portfolio.webstorespring.model.dto.products.response.Subcateg
 import com.example.portfolio.webstorespring.model.entity.products.Subcategory;
 import com.example.portfolio.webstorespring.repositories.products.SubcategoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,31 +17,37 @@ import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SubcategoryService {
 
     private final SubcategoryRepository subcategoryRepository;
     private final CategoryService categoryService;
 
     public List<SubcategoryResponse> getAll() {
+        log.info("Fetching all subcategory.");
         return SubcategoryMapper.mapToDto(subcategoryRepository.findAll());
     }
 
     Set<Subcategory> findAllByNames(Set<String> names) {
+        log.debug("Finding subcategory by names.");
         Set<Subcategory> subcategories = subcategoryRepository.findAllByNames(names);
         if (subcategories.isEmpty()) {
             throw new NotFoundSubcategoriesByNamesException();
         }
+        log.debug("Returning subcategories.");
         return subcategories;
     }
 
     @Transactional
     public SubcategoryResponse save(Long categoryId,
                                     SubcategoryRequest subcategoryRequest) {
+        log.info("Saving subcategory from request: {}", subcategoryRequest);
         Subcategory subcategory = SubcategoryMapper.mapToEntity(
                 subcategoryRequest,
                 categoryService.findById(categoryId)
         );
         subcategoryRepository.save(subcategory);
+        log.info("Saved subcategory.");
         return SubcategoryMapper.mapToDto(subcategory);
     }
 
@@ -48,17 +55,20 @@ public class SubcategoryService {
     public SubcategoryResponse update(Long categoryId,
                                       Long subcategoryId,
                                       SubcategoryRequest subcategoryRequest) {
+        log.info("Updating subcategory for ID: {}, form request: {}", subcategoryId, subcategoryRequest);
         Subcategory foundSubcategory = findById(subcategoryId);
 
         foundSubcategory.setName(subcategoryRequest.name());
         foundSubcategory.setCategory(categoryService.findById(categoryId));
 
         subcategoryRepository.save(foundSubcategory);
+        log.info("Updated subcategory.");
         return SubcategoryMapper.mapToDto(foundSubcategory);
     }
 
-    public void deleteById(Long subcategoryId) {
-        subcategoryRepository.deleteById(subcategoryId);
+    public void deleteById(Long id) {
+        log.info("Deleting subcategory for ID: {}", id);
+        subcategoryRepository.deleteById(id);
     }
 
 
