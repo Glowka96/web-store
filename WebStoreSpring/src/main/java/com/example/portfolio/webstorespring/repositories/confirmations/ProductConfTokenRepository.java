ab51bdef400d70776af5b38ff988bbb33d43d70f
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-31 09:57:37 +0100

    feature: ProductConfTokenRepository and it extends ConfirmationTokenRepository

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/confirmations/ProductConfTokenRepository.java
@@ -0,0 +1,6 @@
+package com.example.portfolio.webstorespring.repositories.confirmations;
+
+import com.example.portfolio.webstorespring.model.entity.confirmations.ProductConfToken;
+
+public interface ProductConfTokenRepository extends ConfirmationTokenRepository<ProductConfToken> {
+}
