8e7ec88eb7d0cf21f0ce1da30113e316361ab3b1
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-31 10:00:05 +0100

    feature: ProductConfTokenService - extends AbstractConfTokenService

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/confirmations/ProductConfTokenService.java
@@ -0,0 +1,27 @@
+package com.example.portfolio.webstorespring.services.confirmations;
+
+import com.example.portfolio.webstorespring.model.entity.confirmations.ProductConfToken;
+import com.example.portfolio.webstorespring.model.entity.confirmations.TokenDetails;
+import com.example.portfolio.webstorespring.model.entity.subscribers.ProductSubscriber;
+import com.example.portfolio.webstorespring.repositories.confirmations.ConfirmationTokenRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductConfTokenService extends AbstractConfTokenService<ProductConfToken, ProductSubscriber> {
+    public ProductConfTokenService(ConfirmationTokenRepository<ProductConfToken> tokenRepository, TokenDetailsService tokenDetailsService) {
+        super(tokenRepository, tokenDetailsService);
+    }
+
+    @Override
+    protected ProductConfToken createTokenEntity(ProductSubscriber relatedEntity, TokenDetails tokenDetails) {
+        return ProductConfToken.builder()
+                .subscriber(relatedEntity)
+                .tokenDetails(tokenDetails)
+                .build();
+    }
+
+    @Override
+    protected ProductSubscriber extractRelatedEntity(ProductConfToken tokenEntity) {
+        return tokenEntity.getSubscriber();
+    }
+}
