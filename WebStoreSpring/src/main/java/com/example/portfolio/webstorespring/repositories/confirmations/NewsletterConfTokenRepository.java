3cbe23f58c8410ee5ffd9b1dec89603892065d51
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-31 09:41:07 +0100

    feature: NewsletterConfTokenRepository extends ConfTokenRepository

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/confirmations/NewsletterConfTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.portfolio.webstorespring.repositories.confirmations;
+
+import com.example.portfolio.webstorespring.model.entity.confirmations.NewsletterConfToken;
+
+public interface NewsletterConfTokenRepository extends ConfirmationTokenRepository<NewsletterConfToken> {
+
+}
