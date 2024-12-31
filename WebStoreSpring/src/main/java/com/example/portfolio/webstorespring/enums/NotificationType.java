b682b0ac620bd295f60dd493071cd8d3b8b8f8ff
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2024-12-31 08:58:25 +0100

    feature: add expiresMinutes in NotificationType;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/enums/NotificationType.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/enums/NotificationType.java
@@ -4,14 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum NotificationType {
-    RESET_PASSWORD("resetPassword"),
-    CONFIRM_EMAIL("confirmEmail"),
-    RECONFIRM_EMAIL("reconfirmEmail"),
-    RESTORE_EMAIL("restoreEmail");
+    RESET_PASSWORD("resetPassword", 15L),
+    CONFIRM_EMAIL("confirmEmail", 15L),
+    RECONFIRM_EMAIL("reconfirmEmail", 15L),
+    RESTORE_EMAIL("restoreEmail", 10_800L);
 
     private final String name;
+    private final Long expiresMinute;
 
-    NotificationType(String name) {
+    NotificationType(String name, Long expiresMinute) {
         this.name = name;
+        this.expiresMinute = expiresMinute;
     }
 }
