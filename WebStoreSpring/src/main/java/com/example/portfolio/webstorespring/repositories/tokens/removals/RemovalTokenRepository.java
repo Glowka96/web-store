53c6d55ff9d40a135afa6a55f11f05f5d2fc490e
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-15 17:59:40 +0100

    fix: type in findByToken;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/tokens/removals/RemovalTokenRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/tokens/removals/RemovalTokenRepository.java
@@ -1,13 +1,12 @@
 package com.example.portfolio.webstorespring.repositories.tokens.removals;
 
-import com.example.portfolio.webstorespring.model.entity.tokens.removals.NewsletterRemovalToken;
 import com.example.portfolio.webstorespring.model.entity.tokens.removals.RemovalToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RemovalTokenRepository<T extends RemovalToken> extends JpaRepository<T, Long> {
-    Optional<NewsletterRemovalToken> findByToken(String token);
+    Optional<T> findByToken(String token);
 
     void deleteByToken(String token);
 }
