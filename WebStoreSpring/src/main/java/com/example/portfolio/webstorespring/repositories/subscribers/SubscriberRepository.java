2bc3f746791ebf7f981d11c312e2122b3593dea3
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-20 15:37:57 +0100

    fix: add @NoRepositoryBean

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/SubscriberRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/SubscriberRepository.java
@@ -2,9 +2,11 @@ package com.example.portfolio.webstorespring.repositories.subscribers;
 
 import com.example.portfolio.webstorespring.model.entity.subscribers.Subscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.Optional;
 
+@NoRepositoryBean
 public interface SubscriberRepository<T extends Subscriber> extends JpaRepository<T, Long> {
 
     Optional<T> findByEmail(String email);
