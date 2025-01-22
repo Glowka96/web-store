800c01b81dc41e2ceec0a16650ae6178862bc2b6
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-22 15:52:07 +0100

    delete: SubscriberRepository.java - clean code; feature: new method in ProductSubscriberRepository

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/NewsletterSubscriberRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/NewsletterSubscriberRepository.java
@@ -1,9 +1,13 @@
 package com.example.portfolio.webstorespring.repositories.subscribers;
 
 import com.example.portfolio.webstorespring.model.entity.subscribers.NewsletterSubscriber;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Set;
 
-public interface NewsletterSubscriberRepository extends SubscriberRepository<NewsletterSubscriber> {
+public interface NewsletterSubscriberRepository extends JpaRepository<NewsletterSubscriber, Long> {
+
     Set<NewsletterSubscriber> findAllByEnabledIsTrue();
+
+    Boolean existsByEmail(String email);
 }
--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/ProductSubscriberRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/ProductSubscriberRepository.java
@@ -1,6 +1,20 @@
 package com.example.portfolio.webstorespring.repositories.subscribers;
 
 import com.example.portfolio.webstorespring.model.entity.subscribers.ProductSubscriber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductSubscriberRepository extends SubscriberRepository<ProductSubscriber> {
+import java.util.Optional;
+
+public interface ProductSubscriberRepository extends JpaRepository<ProductSubscriber, Long> {
+
+    Optional<ProductSubscriber> findByEmail(String email);
+
+    @Query("""
+            SELECT subscriber FROM ProductSubscriber subscriber
+            JOIN subscriber.subscription subscription
+            WHERE subscriber.email = :email
+            """)
+    Optional<ProductSubscriber> findSubscriberAndSubscriptionByEmail(@Param("email") String email);
 }
