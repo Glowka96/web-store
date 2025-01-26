9214d349614a50e81cf41492fd916978592910af
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-26 17:22:35 +0100

    feature: get and delete ByToken methods; change: correct method name;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/tokens/removals/SingleProductRemovalTokenService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/tokens/removals/SingleProductRemovalTokenService.java
@@ -1,5 +1,6 @@
 package com.example.portfolio.webstorespring.services.tokens.removals;
 
+import com.example.portfolio.webstorespring.exceptions.ResourceNotFoundException;
 import com.example.portfolio.webstorespring.model.entity.subscribers.ProductSubscriber;
 import com.example.portfolio.webstorespring.model.entity.tokens.removals.SingleProductRemovalToken;
 import com.example.portfolio.webstorespring.repositories.tokens.removals.SingleProductRemovalRepository;
@@ -12,10 +13,19 @@ public class SingleProductRemovalTokenService {
 
     private final SingleProductRemovalRepository removalTokenRepository;
 
-    public SingleProductRemovalToken create(ProductSubscriber subscriber, Long productId) {
+    public SingleProductRemovalToken getByToken(String token) {
+        return removalTokenRepository.findByToken(token)
+                .orElseThrow(()-> new ResourceNotFoundException("Removal token","token", token));
+    }
+
+    public SingleProductRemovalToken save(ProductSubscriber subscriber, Long productId) {
         return removalTokenRepository.save(SingleProductRemovalToken.builder()
                 .subscriber(subscriber)
                 .productId(productId)
                 .build());
+    }
+
+    public void deleteByToken(String token) {
+        removalTokenRepository.deleteByToken(token);
     }
 }
