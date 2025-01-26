dd4c87756d697abd3402785505ce3924cc0fddc7
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-26 17:30:14 +0100

    feature: findByIdAndSubscriberEmail custom query; change: LEFT JOIN to JOIN;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/ProductSubscriptionRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/ProductSubscriptionRepository.java
@@ -3,6 +3,7 @@ package com.example.portfolio.webstorespring.repositories.subscribers;
 import com.example.portfolio.webstorespring.model.entity.subscribers.ProductSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,7 +11,15 @@ public interface ProductSubscriptionRepository extends JpaRepository<ProductSubs
 
     @Query("""
         SELECT ps FROM ProductSubscription ps
-        LEFT JOIN ps.productSubscribers s ON s.enabled = true
+        JOIN ps.productSubscribers s ON s.enabled = true
+        WHERE ps.product.id = :productId
 """)
-    Optional<ProductSubscription> findByProductSubscribersEnabled(Long productId);
+    Optional<ProductSubscription> findByProductSubscribersEnabled(@Param("productId") Long productId);
+
+    @Query("""
+        SELECT ps FROM ProductSubscription ps
+        JOIN ps.productSubscribers s
+        WHERE ps.product.id = :productId AND s.email = :email
+""")
+    Optional<ProductSubscription> findByIdAndSubscriberEmail(@Param("productId") Long productId, @Param("email") String email);
 }
