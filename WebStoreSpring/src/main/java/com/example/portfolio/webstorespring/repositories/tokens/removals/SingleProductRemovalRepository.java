1ec885d2718f09b88ee07a0ca546d189a72f56be
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-26 17:21:39 +0100

    feature: find and delete ByToken methods, for find custom query;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/tokens/removals/SingleProductRemovalRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/tokens/removals/SingleProductRemovalRepository.java
@@ -2,6 +2,18 @@ package com.example.portfolio.webstorespring.repositories.tokens.removals;
 
 import com.example.portfolio.webstorespring.model.entity.tokens.removals.SingleProductRemovalToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SingleProductRemovalRepository extends JpaRepository<SingleProductRemovalToken, Long> {
+
+    @Query("""
+            SELECT token FROM SingleProductRemovalToken token
+            LEFT JOIN FETCH token.subscriber
+            WHERE token.token = :token
+            """)
+    Optional<SingleProductRemovalToken> findByToken(@Param("token") String token);
+    void deleteByToken(String token);
 }
