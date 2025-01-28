4d200b20f4343282809665c853d2d59d016bedf4
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-28 18:34:58 +0100

    feature: deleteFromSingleProduct method; change: simplify method name, correct service name, void to Map<String, Object>

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/unsubscribes/UnsubscribeProduct.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/unsubscribes/UnsubscribeProduct.java
@@ -2,27 +2,43 @@ package com.example.portfolio.webstorespring.services.unsubscribes;
 
 import com.example.portfolio.webstorespring.model.entity.subscribers.ProductSubscriber;
 import com.example.portfolio.webstorespring.model.entity.tokens.removals.ProductRemovalToken;
+import com.example.portfolio.webstorespring.model.entity.tokens.removals.SingleProductRemovalToken;
 import com.example.portfolio.webstorespring.services.subscribers.ProductSubscriberService;
+import com.example.portfolio.webstorespring.services.subscribers.ProductSubscriptionService;
 import com.example.portfolio.webstorespring.services.tokens.removals.ProductRemovalTokenService;
+import com.example.portfolio.webstorespring.services.tokens.removals.SingleProductRemovalTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UnsubscribeProduct {
 
-    private final ProductRemovalTokenService removalTokenService;
+    private final ProductRemovalTokenService productRemovalTokenService;
+    private final SingleProductRemovalTokenService singleProductRemovalTokenService;
     private final ProductSubscriberService subscriberService;
+    private final ProductSubscriptionService subscriptionService;
 
     @Transactional
-    public void deleteSubscriberAndHisSubscriptions(String token) {
-        ProductRemovalToken productRemovalToken = removalTokenService.get(token);
+    public Map<String, Object> deleteAllSubscriptions(String token) {
+        ProductRemovalToken productRemovalToken = productRemovalTokenService.get(token);
         ProductSubscriber subscriber = subscriberService.findWithSubscriptionById(productRemovalToken.getId());
         subscriber.getSubscription().forEach(
                 subscription -> subscription.removeSubscriber(subscriber)
         );
         subscriberService.delete(subscriber);
-        removalTokenService.delete(token);
+        productRemovalTokenService.delete(token);
+        return Map.of("message", "Your all subscription was removed");
+    }
+
+    @Transactional
+    public Map<String, Object> deleteFromSingleProduct(String token) {
+        SingleProductRemovalToken singleProductRemovalToken = singleProductRemovalTokenService.getByToken(token);
+        Map<String, Object> result = subscriptionService.removeForSingleProduct(singleProductRemovalToken.getSubscriber(), singleProductRemovalToken.getProductId());
+        productRemovalTokenService.delete(token);
+        return result;
     }
 }
