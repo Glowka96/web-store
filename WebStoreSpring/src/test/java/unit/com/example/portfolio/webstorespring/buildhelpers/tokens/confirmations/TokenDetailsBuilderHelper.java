d5d1a12666eccc186084a6ddf88851b0310ffb56
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-30 17:01:08 +0100

    test: remove ConfirmationTokenBuilderHelper.java, add TokenDetailsBuilderHelper;

---
+++ WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/buildhelpers/tokens/confirmations/TokenDetailsBuilderHelper.java
@@ -0,0 +1,26 @@
+package com.example.portfolio.webstorespring.buildhelpers.tokens.confirmations;
+
+import com.example.portfolio.webstorespring.model.entity.tokens.confirmations.TokenDetails;
+import com.natpryce.makeiteasy.Instantiator;
+import com.natpryce.makeiteasy.Property;
+
+import java.time.LocalDateTime;
+
+import static com.example.portfolio.webstorespring.buildhelpers.DateForTestBuilderHelper.LOCAL_DATE_TIME;
+
+public class TokenDetailsBuilderHelper {
+
+    public static final Property<TokenDetails, Long> ID = new Property<>();
+    public static final Property<TokenDetails, String> TOKEN_NAME = new Property<>();
+    public static final Property<TokenDetails, LocalDateTime> CREATED_AT = new Property<>();
+    public static final Property<TokenDetails, LocalDateTime> EXPIRES_AT = new Property<>();
+    public static final Property<TokenDetails, LocalDateTime> CONFIRMED_AT = new Property<>();
+
+    public static final Instantiator<TokenDetails> BASIC_TOKEN_DETAILS = lookup ->
+            TokenDetails.builder()
+                    .token(lookup.valueOf(TOKEN_NAME, "token"))
+                    .createdAt(lookup.valueOf(CREATED_AT, LOCAL_DATE_TIME))
+                    .confirmedAt(lookup.valueOf(CONFIRMED_AT, LOCAL_DATE_TIME.plusMinutes(10)))
+                    .expiresAt(lookup.valueOf(EXPIRES_AT, LOCAL_DATE_TIME.plusMinutes(15)))
+                    .build();
+}
