a42ebf89295397422945c0c48ad5780965bc6617
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-01-30 19:51:58 +0100

    test: add more notification strategies;

--- WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/services/email/strategy/NotificationStrategyFactoryTest.java
+++ WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/services/email/strategy/NotificationStrategyFactoryTest.java
@@ -1,6 +1,8 @@
 package com.example.portfolio.webstorespring.services.email.strategy;
 
 import com.example.portfolio.webstorespring.enums.NotificationType;
+import com.example.portfolio.webstorespring.services.emails.strategy.NotificationStrategy;
+import com.example.portfolio.webstorespring.services.emails.strategy.NotificationStrategyFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,32 +20,71 @@ class NotificationStrategyFactoryTest {
 
     private NotificationStrategyFactory underTest;
     @Mock
-    private NotificationStrategy reconfirmEmailStrategy;
+    private NotificationStrategy resetPasswordStrategy;
     @Mock
     private NotificationStrategy confirmEmailStrategy;
     @Mock
-    private NotificationStrategy resetPasswordStrategy;
+    private NotificationStrategy reconfirmEmailStrategy;
+    @Mock
+    private NotificationStrategy restoreEmailStrategy;
+    @Mock
+    private NotificationStrategy confirmNewsletterEmailStrategy;
+    @Mock
+    private NotificationStrategy reconfirmNewsletterEmailStrategy;
+    @Mock
+    private NotificationStrategy welcomeNewsletterEmailStrategy;
+    @Mock
+    private NotificationStrategy confirmProfileSubscriptionEmailStrategy;
+    @Mock
+    private NotificationStrategy reconfirmProfileSubscriptionEmailStrategy;
+    @Mock
+    private NotificationStrategy welcomeProductSubscriptionEmailStrategy;
 
-    @BeforeEach
-    void setUp() {
-        when(reconfirmEmailStrategy.getNotificationType()).thenReturn(NotificationType.RECONFIRM_EMAIL);
-        when(confirmEmailStrategy.getNotificationType()).thenReturn(NotificationType.CONFIRM_EMAIL);
-        when(resetPasswordStrategy.getNotificationType()).thenReturn(NotificationType.RESET_PASSWORD);
-
-        Set<NotificationStrategy> strategies = Set.of(reconfirmEmailStrategy, confirmEmailStrategy, resetPasswordStrategy);
-
-        underTest = new NotificationStrategyFactory(strategies);
-    }
 
     @ParameterizedTest
     @EnumSource(value = NotificationType.class, names = {
-            "RECONFIRM_EMAIL",
+            "RESET_PASSWORD",
             "CONFIRM_EMAIL",
-            "RESET_PASSWORD"
+            "RECONFIRM_EMAIL",
+            "CONFIRM_NEWSLETTER",
+            "RECONFIRM_NEWSLETTER",
+            "WELCOME_NEWSLETTER",
+            "CONFIRM_PRODUCT_SUBSCRIPTION",
+            "RECONFIRM_PRODUCT_SUBSCRIPTION",
+            "WELCOME_PRODUCT_SUBSCRIPTION",
     })
     void shouldFindNotificationStrategy_whenNotificationTypeIsGiven(NotificationType notificationType) {
         NotificationStrategy notificationStrategy = underTest.findNotificationStrategy(notificationType);
 
         assertNotNull(notificationStrategy);
+    }
+
+    @BeforeEach
+    void setUp() {
+        when(resetPasswordStrategy.getNotificationType()).thenReturn(NotificationType.RESET_PASSWORD);
+        when(confirmEmailStrategy.getNotificationType()).thenReturn(NotificationType.CONFIRM_EMAIL);
+        when(reconfirmEmailStrategy.getNotificationType()).thenReturn(NotificationType.RECONFIRM_EMAIL);
+        when(restoreEmailStrategy.getNotificationType()).thenReturn(NotificationType.RESTORE_EMAIL);
+        when(confirmNewsletterEmailStrategy.getNotificationType()).thenReturn(NotificationType.CONFIRM_NEWSLETTER);
+        when(reconfirmNewsletterEmailStrategy.getNotificationType()).thenReturn(NotificationType.RECONFIRM_NEWSLETTER);
+        when(welcomeNewsletterEmailStrategy.getNotificationType()).thenReturn(NotificationType.WELCOME_NEWSLETTER);
+        when(confirmProfileSubscriptionEmailStrategy.getNotificationType()).thenReturn(NotificationType.CONFIRM_PRODUCT_SUBSCRIPTION);
+        when(reconfirmProfileSubscriptionEmailStrategy.getNotificationType()).thenReturn(NotificationType.RECONFIRM_PRODUCT_SUBSCRIPTION);
+        when(welcomeProductSubscriptionEmailStrategy.getNotificationType()).thenReturn(NotificationType.WELCOME_PRODUCT_SUBSCRIPTION);
+
+        Set<NotificationStrategy> strategies = Set.of(
+                resetPasswordStrategy,
+                confirmEmailStrategy,
+                reconfirmEmailStrategy,
+                restoreEmailStrategy,
+                confirmNewsletterEmailStrategy,
+                reconfirmNewsletterEmailStrategy,
+                welcomeNewsletterEmailStrategy,
+                confirmProfileSubscriptionEmailStrategy,
+                reconfirmProfileSubscriptionEmailStrategy,
+                welcomeProductSubscriptionEmailStrategy
+                );
+
+        underTest = new NotificationStrategyFactory(strategies);
     }
 }
