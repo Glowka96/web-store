feae02edc4f19af90b7f3c020e7cf961538c5a4c
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-08 18:28:14 +0100

    test: ProductSubscriber builder helper - using MakeItEasy

---
+++ WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/buildhelpers/subscribers/ProductSubscriberBuilderHelper.java
@@ -0,0 +1,35 @@
+package com.example.portfolio.webstorespring.buildhelpers.subscribers;
+
+import com.example.portfolio.webstorespring.models.entity.subscribers.ProductSubscriber;
+import com.example.portfolio.webstorespring.models.entity.subscribers.ProductSubscription;
+import com.example.portfolio.webstorespring.models.entity.tokens.removals.SingleProductRemovalToken;
+import com.natpryce.makeiteasy.Instantiator;
+import com.natpryce.makeiteasy.Property;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import static com.example.portfolio.webstorespring.buildhelpers.DateForTestBuilderHelper.LOCAL_DATE_TIME;
+
+public class ProductSubscriberBuilderHelper {
+
+    public static final Property<ProductSubscriber, Long> ID = new Property<>();
+    public static final Property<ProductSubscriber, String> EMAIL = new Property<>();
+    public static final Property<ProductSubscriber, Boolean> ENABLED = new Property<>();
+    public static final Property<ProductSubscriber, LocalDateTime> CREATED_AT = new Property<>();
+    public static final Property<ProductSubscriber, List<SingleProductRemovalToken>> TOKENS = new Property<>();
+    public static final Property<ProductSubscriber, Set<ProductSubscription>> SUBSCRIPTIONS = new Property<>();
+
+    private static final String PRODUCT_SUBSCRIBER_EMAIL = "test@test.pl";
+
+    public static final Instantiator<ProductSubscriber> BASIC_PRODUCT_SUBSCRIBER = lookup ->
+            ProductSubscriber.builder()
+                    .id(lookup.valueOf(ID, 1L))
+                    .email(lookup.valueOf(EMAIL, PRODUCT_SUBSCRIBER_EMAIL))
+                    .enabled(lookup.valueOf(ENABLED, Boolean.TRUE))
+                    .createdAt(lookup.valueOf(CREATED_AT, LOCAL_DATE_TIME))
+                    .tokens(lookup.valueOf(TOKENS, List.of()))
+                    .subscriptions(lookup.valueOf(SUBSCRIPTIONS, Set.of()))
+                    .build();
+}
