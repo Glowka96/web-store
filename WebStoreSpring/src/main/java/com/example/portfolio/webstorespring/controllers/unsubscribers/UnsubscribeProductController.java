83387bc08938e73bc1723ce2311b8f224b8ae05d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-09 11:34:27 +0100

    change: add "/confirm" to URI in controllers;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/controllers/unsubscribers/UnsubscribeProductController.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/controllers/unsubscribers/UnsubscribeProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/unsubscribe-product-subscription")
+@RequestMapping("/api/v1/unsubscribe-product-subscription/confirm")
 @RequiredArgsConstructor
 public class UnsubscribeProductController {
 
