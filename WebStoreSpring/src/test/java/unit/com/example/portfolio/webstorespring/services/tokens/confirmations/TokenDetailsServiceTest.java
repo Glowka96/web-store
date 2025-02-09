790cbb7fb9d719cdfd5286ebc61968d44cb62d2a
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-09 19:54:17 +0100

    test: TokenDetailsService method

---
+++ WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/services/tokens/confirmations/TokenDetailsServiceTest.java
@@ -0,0 +1,90 @@
+package com.example.portfolio.webstorespring.services.tokens.confirmations;
+
+import com.example.portfolio.webstorespring.exceptions.TokenConfirmedException;
+import com.example.portfolio.webstorespring.exceptions.TokenExpiredException;
+import com.example.portfolio.webstorespring.models.entity.tokens.confirmations.TokenDetails;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+
+import static com.example.portfolio.webstorespring.buildhelpers.DateForTestBuilderHelper.LOCAL_DATE_TIME;
+import static com.example.portfolio.webstorespring.buildhelpers.DateForTestBuilderHelper.ZONED_DATE_TIME;
+import static com.example.portfolio.webstorespring.buildhelpers.tokens.confirmations.TokenDetailsBuilderHelper.*;
+import static com.natpryce.makeiteasy.MakeItEasy.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TokenDetailsServiceTest {
+
+    @Mock
+    private Clock clock;
+    @InjectMocks
+    private TokenDetailsService underTest;
+
+    @Test
+    void shouldCreate() {
+        setupClock();
+
+        TokenDetails tokenDetails = underTest.create(10_800L);
+
+        assertNotNull(tokenDetails);
+    }
+
+    @Test
+    void shouldValidate_whenNotConfirmedAndNoExpired() {
+        TokenDetails tokenDetails = make(a(BASIC_TOKEN_DETAILS)
+                .but(withNull(CONFIRMED_AT)));
+        setupClock();
+
+        underTest.validate(tokenDetails);
+    }
+
+    @Test
+    void willThrowTokenConfirmedException_whenTokenIsConfirmed() {
+        TokenDetails tokenDetails = make(a(BASIC_TOKEN_DETAILS));
+
+        assertThrows(TokenConfirmedException.class, ()-> underTest.validate(tokenDetails));
+    }
+
+    @Test
+    void willThrowTokenExpiredException_whenTokenIsExpired() {
+        TokenDetails tokenDetails = make(a(BASIC_TOKEN_DETAILS)
+                .but(withNull(CONFIRMED_AT))
+                .but(with(EXPIRES_AT, LOCAL_DATE_TIME.minusMinutes(1))));
+        setupClock();
+
+        assertThrows(TokenExpiredException.class, ()-> underTest.validate(tokenDetails));
+    }
+
+    @Test
+    void setConfirmedAt() {
+        TokenDetails tokenDetails = make(a(BASIC_TOKEN_DETAILS)
+                .but(withNull(CONFIRMED_AT)));
+        setupClock();
+
+        underTest.setConfirmedAt(tokenDetails);
+
+        assertNotNull(tokenDetails.getConfirmedAt());
+        assertEquals(tokenDetails.getConfirmedAt(), LOCAL_DATE_TIME);
+    }
+
+    @Test
+    void shouldValidateTokenExpired() {
+        TokenDetails tokenDetails = make(a(BASIC_TOKEN_DETAILS));
+        setupClock();
+
+        boolean result = underTest.isTokenExpired(tokenDetails);
+
+        assertFalse(result);
+    }
+
+    private void setupClock() {
+        when(clock.getZone()).thenReturn(ZONED_DATE_TIME.getZone());
+        when(clock.instant()).thenReturn(ZONED_DATE_TIME.toInstant());
+    }
+}
