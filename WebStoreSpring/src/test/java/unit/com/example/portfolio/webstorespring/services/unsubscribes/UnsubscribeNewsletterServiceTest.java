0979b3b4ec1ec4d6007f2a88a80105901b70653e
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-09 14:49:57 +0100

    test: UnsubscribeNewsletterService method;

---
+++ WebStoreSpring/src/test/java/unit/com/example/portfolio/webstorespring/services/unsubscribes/UnsubscribeNewsletterServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.portfolio.webstorespring.services.unsubscribes;
+
+import com.example.portfolio.webstorespring.models.dto.ResponseMessageDTO;
+import com.example.portfolio.webstorespring.models.entity.tokens.removals.NewsletterRemovalToken;
+import com.example.portfolio.webstorespring.services.subscribers.NewsletterSubscriberService;
+import com.example.portfolio.webstorespring.services.tokens.removals.NewsletterRemovalTokenService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.example.portfolio.webstorespring.buildhelpers.tokens.removals.NewsletterRemovalTokenBuilderHelper.createNewsletterRemovalTokenWithEnabledSubscriber;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UnsubscribeNewsletterServiceTest {
+
+    @Mock
+    private NewsletterRemovalTokenService newsletterRemovalTokenService;
+    @Mock
+    private NewsletterSubscriberService newsletterSubscriberService;
+    @InjectMocks
+    private UnsubscribeNewsletterService underTest;
+
+    @Test
+    void shouldDeleteSubscription() {
+        NewsletterRemovalToken removalToken = createNewsletterRemovalTokenWithEnabledSubscriber();
+        ResponseMessageDTO response = new ResponseMessageDTO("Your subscription was removed");
+
+        given(newsletterRemovalTokenService.getByToken(anyString())).willReturn(removalToken);
+
+        ResponseMessageDTO result = underTest.deleteSubscription("token123");
+
+        verify(newsletterSubscriberService, times(1)).deleteById(removalToken.getId());
+        verify(newsletterRemovalTokenService, times(1)).delete(removalToken.getToken());
+        assertEquals(response, result);
+    }
+}
