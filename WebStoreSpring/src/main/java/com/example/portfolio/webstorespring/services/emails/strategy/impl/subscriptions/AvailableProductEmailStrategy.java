dad8f1fb17360b351b26d5a8f23ebf3bb9ec9c3d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-23 15:45:01 +0100

    feature: EmailStrategy for EmailType.AVAILABLE_PRODUCT

---
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/emails/strategy/impl/subscriptions/AvailableProductEmailStrategy.java
@@ -0,0 +1,39 @@
+package com.example.portfolio.webstorespring.services.emails.strategy.impl.subscriptions;
+
+import com.example.portfolio.webstorespring.configs.providers.CorsProvider;
+import com.example.portfolio.webstorespring.enums.EmailType;
+import com.example.portfolio.webstorespring.services.emails.strategy.EmailStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AvailableProductEmailStrategy implements EmailStrategy {
+
+    private final CorsProvider corsProvider;
+
+    @Override
+    public EmailType getNotificationType() {
+        return EmailType.AVAILABLE_PRODUCT;
+    }
+
+    @Override
+    public String getEmailTitle() {
+        return "Your product is Now Back in Stock!";
+    }
+
+    @Override
+    public String getEmailMessage() {
+        return """
+                Hello,
+                We are pleased to inform you that the product: %s is now available on our webshop.
+                You can visit our store to make a purchase at your convenience.
+                Visit the shop:
+                """ +
+                corsProvider.getAllowedOriginPatterns() + "/api/v1/products/%s" +
+                """
+                Best regards,
+                The Webshop Team
+                """;
+    }
+}
