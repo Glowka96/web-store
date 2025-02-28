0f702c11877275a704ba2e6c4fba27ebfe6db87d
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-02-28 19:02:39 +0100

    fix: add missed @EntityListeners

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/models/entity/subscribers/ProductSubscriber.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/models/entity/subscribers/ProductSubscriber.java
@@ -4,6 +4,7 @@ import com.example.portfolio.webstorespring.models.entity.tokens.removals.Single
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -16,6 +17,7 @@ import java.util.Set;
 @Getter
 @Setter
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSubscriber implements OwnerConfToken, Subscriber {
