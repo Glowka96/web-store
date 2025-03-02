38b7a5f89a6cdf533eadf7af685534775a1d43a1
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-03-02 18:38:15 +0100

    change: add findFutureMessages jpql;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/NewsletterMessageRepository.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/repositories/subscribers/NewsletterMessageRepository.java
@@ -2,6 +2,15 @@ package com.example.portfolio.webstorespring.repositories.subscribers;
 
 import com.example.portfolio.webstorespring.models.entity.subscribers.NewsletterMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NewsletterMessageRepository extends JpaRepository<NewsletterMessage, Long> {
+
+    @Query("""
+            SELECT n FROM NewsletterMessage n
+            WHERE n.sendDate > CURRENT_TIMESTAMP
+            """)
+    List<NewsletterMessage> findFutureMessages();
 }
