9d13c0fa04b743980a0f127d25eb3523869aa9a7
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-03-04 20:00:58 +0100

    test: add second test case - not save, correct test's name, delete data after tests;

--- WebStoreSpring/src/test/java/integration/com/example/portfolio/webstorespring/controllers/subscribers/NewsletterMessageControllerIT.java
+++ WebStoreSpring/src/test/java/integration/com/example/portfolio/webstorespring/controllers/subscribers/NewsletterMessageControllerIT.java
@@ -3,19 +3,29 @@ package com.example.portfolio.webstorespring.controllers.subscribers;
 import com.example.portfolio.webstorespring.controllers.AbstractAuthControllerIT;
 import com.example.portfolio.webstorespring.models.dto.ResponseMessageDTO;
 import com.example.portfolio.webstorespring.models.dto.subscribers.NewsletterMessageRequest;
+import com.example.portfolio.webstorespring.repositories.subscribers.NewsletterMessageRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NewsletterMessageControllerIT extends AbstractAuthControllerIT {
 
+    @Autowired
+    private NewsletterMessageRepository repository;
+
+    @AfterEach
+    void deleteTestData() {
+        repository.deleteAll();
+    }
+
     @Test
-    void shouldSave() {
+    void shouldSave_forAuthenticatedAdmin_thenStatusIsCreated() {
         NewsletterMessageRequest request = new NewsletterMessageRequest(
                 "Test message",
                 null
@@ -31,6 +41,26 @@ class NewsletterMessageControllerIT extends AbstractAuthControllerIT {
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         assertNotNull(response.getBody());
+        assertEquals("Newsletter message send to enabled subscribers", response.getBody().message());
+    }
+
+    @Test
+    void shouldNotSave_forAuthenticatedUser_thenStatusIsForbidden() {
+        NewsletterMessageRequest request = new NewsletterMessageRequest(
+                "Test message",
+                null
+        );
+        HttpEntity<NewsletterMessageRequest> httpEntity = new HttpEntity<>(request, getHttpHeaderWithUserToken());
+
+        ResponseEntity<ResponseMessageDTO> response = restTemplate.exchange(
+                localhostAdminUri + "/newsletters/messages",
+                HttpMethod.POST,
+                httpEntity,
+                ResponseMessageDTO.class
+        );
+
+        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+        assertNull(response.getBody());
 
     }
 }
