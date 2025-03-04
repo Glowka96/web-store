ebf23fd7ab240108604709a4d044ebfc1f3e4cc7
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-03-04 19:51:57 +0100

    change: add validation annotation to sendDate;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/models/dto/subscribers/NewsletterMessageRequest.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/models/dto/subscribers/NewsletterMessageRequest.java
@@ -1,5 +1,6 @@
 package com.example.portfolio.webstorespring.models.dto.subscribers;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 public record NewsletterMessageRequest(
         @NotBlank(message = "The message can't be blank")
         String message,
+        @Future(message = "The send date must be in the future")
         LocalDateTime sendDate
 ) {
 }
