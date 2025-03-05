363b9886b8da068a72362f9244e11faaeca93236
Sebastian Głowacki <glowackisebastian.it@gmail.com>
2025-03-05 04:08:13 +0100

    change: clean code and add logs;

--- WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/subscribers/NewsletterSchedulerService.java
+++ WebStoreSpring/src/main/java/com/example/portfolio/webstorespring/services/subscribers/NewsletterSchedulerService.java
@@ -3,6 +3,7 @@ package com.example.portfolio.webstorespring.services.subscribers;
 import com.example.portfolio.webstorespring.models.entity.subscribers.NewsletterMessage;
 import com.example.portfolio.webstorespring.services.emails.notifications.NotifyNewsletterSubscriberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.util.concurrent.ScheduledFuture;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NewsletterSchedulerService {
 
     private final NewsletterMessageService newsletterMessageService;
@@ -26,18 +28,16 @@ public class NewsletterSchedulerService {
 
     @Scheduled(fixedRate = 3_600_000, initialDelay = 10_000)
     public void checkAndScheduleTasks() {
+        log.info("Checking schedule tasks");
         List<NewsletterMessage> futureMessages = newsletterMessageService.getAllFutureMessages();
-
-        for (NewsletterMessage message : futureMessages) {
-            scheduleTask(message);
-        }
+        futureMessages.forEach(this::scheduleTask);
     }
 
     private void scheduleTask(NewsletterMessage newsletterMessage) {
         if (scheduledTasks.containsKey(newsletterMessage.getId())) {
             return;
         }
-
+        log.debug("Adding new schedule tasks");
         ScheduledFuture<?> future = taskScheduler.schedule(
                 () -> executeTask(newsletterMessage), newsletterMessage.getSendDate().atZone(clock.getZone()).toInstant()
         );
@@ -45,6 +45,7 @@ public class NewsletterSchedulerService {
     }
 
     private void executeTask(NewsletterMessage newsletterMessage) {
+        log.info("Execute notify newsletter: {}", newsletterMessage.getId());
         notifyNewsletterSubscriberService.notifyEnabledSubscribers(newsletterMessage.getMessage());
         scheduledTasks.remove(newsletterMessage.getId());
     }
